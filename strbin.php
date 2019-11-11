3cbf8161fb554ad9dc0802ef17c47d0e297fd12b
T!M Greller <timlg07@gmail.com>
2019-11-11 22:12:29 +0100

    added str2bin conversion

--- strbin.php
+++ strbin.php
@@ -2,7 +2,18 @@
     
     class StrBinConverter
     {
-        
+        public static function str2bin( $string )
+        {
+            $bin = [];            
+            // reduce function calls
+            $len = strlen( $string );
+            // iterate the bytes of the string
+            for( $i=0; $i<$len; $i++ ){
+                // convert to binary and add to array
+                array_push( $bin, decbin(ord( $string[$i] )) );
+            }
+            return $bin;
+        }
     }
 
 ?>
