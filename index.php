c5f3f10cbb548fc8d4d69a6006c246bfa49ee09e
T!M Greller <timlg07@gmail.com>
2019-11-15 20:42:54 +0100

    created index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <a href="select.php">Abfragen</a> <br>
+        <a href="add.php">Hinzufuegen</a> <br>
+        <br>
+        <a href="https://tim-greller.000webhostapp.com/pw/">Mit ssh verschluesseln.</a>
+    </body>
+</html>
